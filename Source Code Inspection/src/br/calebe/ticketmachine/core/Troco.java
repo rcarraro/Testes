be69463755e80fd4e13c8063b6a9d710cdba4d67
rcarraro <42623544+rcarraro@users.noreply.github.com>
2023-09-22 21:29:52 -0300

    Fix no Troco

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -14,31 +14,37 @@ class Troco {
         papeisMoeda = new PapelMoeda[6];
         int count = 0;
         while (valor % 100 != 0) {
+            valor = valor%100
             count++;
         }
         papeisMoeda[5] = new PapelMoeda(100, count);
         count = 0;
         while (valor % 50 != 0) {
+            valor = valor%50
             count++;
         }
         papeisMoeda[4] = new PapelMoeda(50, count);
         count = 0;
         while (valor % 20 != 0) {
+            valor = valor%20
             count++;
         }
         papeisMoeda[3] = new PapelMoeda(20, count);
         count = 0;
         while (valor % 10 != 0) {
+            valor = valor%10
             count++;
         }
         papeisMoeda[2] = new PapelMoeda(10, count);
         count = 0;
         while (valor % 5 != 0) {
+            valor = valor%5
             count++;
         }
-        papeisMoeda[1] = new PapelMoeda(5, count);
+        papeisMoeda[0] = new PapelMoeda(5, count);
         count = 0;
         while (valor % 2 != 0) {
+            valor = valor%2
             count++;
         }
         papeisMoeda[1] = new PapelMoeda(2, count);
@@ -58,8 +64,8 @@ class Troco {
 
         @Override
         public boolean hasNext() {
-            for (int i = 6; i >= 0; i++) {
-                if (troco.papeisMoeda[i] != null) {
+            for (int i = 5; i >= 0; i--) {
+                if (troco.papeisMoeda[i].count != 0) {
                     return true;
                 }
             }
@@ -69,7 +75,7 @@ class Troco {
         @Override
         public PapelMoeda next() {
             PapelMoeda ret = null;
-            for (int i = 6; i >= 0 && ret != null; i++) {
+            for (int i = 5; i >= 0 && ret != null; i--) {
                 if (troco.papeisMoeda[i] != null) {
                     ret = troco.papeisMoeda[i];
                     troco.papeisMoeda[i] = null;
