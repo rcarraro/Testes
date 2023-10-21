3d9fb07d2a85c19dd25b3f9873f7054cd4067983
Rcarraro <carrarorafa@gmail.com>
2023-10-21 11:50:10 -0300

    primeira tentativa de teste

---
+++ src/test/java/TicketMachineTest.java
@@ -0,0 +1,43 @@
+package br.calebe.ticketmachine.core;
+
+import br.calebe.ticketmachine.exception.PapelMoedaInvalidaException;
+import br.calebe.ticketmachine.exception.SaldoInsuficienteException;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TicketMachineTest {
+
+    private TicketMachine ticketMachine;
+
+    @Before
+    public void setUp() {
+        ticketMachine = new TicketMachine(50);  
+    }
+
+    @Test
+    public void testInserirValidQuantia() throws PapelMoedaInvalidaException {
+        ticketMachine.inserir(10);  
+        assertEquals(10, ticketMachine.getSaldo());
+    }
+
+    @Test(expected = PapelMoedaInvalidaException.class)
+    public void testInserirInvalidQuantia() throws PapelMoedaInvalidaException {
+        ticketMachine.inserir(7);  
+    }
+
+    @Test
+    public void testImprimirTicketWithSufficientBalance() throws SaldoInsuficienteException {
+        ticketMachine.inserir(50);  
+        String ticket = ticketMachine.imprimir();
+        assertEquals("*** R$ 50,00 ****\n", ticket);
+        assertEquals(0, ticketMachine.getSaldo());
+    }
+
+    @Test(expected = SaldoInsuficienteException.class)
+    public void testImprimirTicketWithInsufficientBalance() throws SaldoInsuficienteException {
+        ticketMachine.inserir(30);  
+        ticketMachine.imprimir();
+    }
+}
