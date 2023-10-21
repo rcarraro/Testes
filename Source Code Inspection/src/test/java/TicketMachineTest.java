cc3a04885302ca79290b6d42a9ec819c232ed44c
Rcarraro <carrarorafa@gmail.com>
2023-10-21 14:14:19 -0300

    teste

--- Source Code Inspection/src/test/java/TicketMachineTest.java
+++ Source Code Inspection/src/test/java/TicketMachineTest.java
@@ -6,8 +6,7 @@ import org.junit.Test;
 import org.junit.Before;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.Assert.assertThrows;
 
 public class TicketMachineTest {
 
