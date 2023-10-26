0aa6fa4d8108f95fd47094114d5d8980437305b5
Rafael Martins <42623544+rcarraro@users.noreply.github.com>
2023-10-26 20:57:09 -0300

    Update TrocoTest.java

--- Source Code Inspection/src/test/java/TrocoTest.java
+++ Source Code Inspection/src/test/java/TrocoTest.java
@@ -1,28 +1,32 @@
 package br.calebe.ticketmachine.core;
 
-import br.calebe.ticketmachine.exception.PapelMoedaInvalidaException;
-import br.calebe.ticketmachine.exception.SaldoInsuficienteException;
-import org.junit.Test;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
 
 public class TrocoTest {
 
     private Troco troco;
-  
+
     @Before
     public void setUp() {
-        troco = new Troco(256);  
+        troco = new Troco(256);
     }
+
     @Test
-    public void testTroco() throws PapelMoedaInvalidaException {
-        assertEquals(2, troco.papeisMoeda[5].getQuantidade());
-        assertEquals(1, troco.papeisMoeda[4].getQuantidade());
-        assertEquals(0, troco.papeisMoeda[3].getQuantidade());
-        assertEquals(0, troco.papeisMoeda[2].getQuantidade());
-        assertEquals(0, troco.papeisMoeda[1].getQuantidade());
-        assertEquals(3, troco.papeisMoeda[0].getQuantidade());
+    public void testTrocoCalculation() {
+        assertEquals(2, troco.papeisMoeda[5].getQuantidade());  
+        assertEquals(1, troco.papeisMoeda[4].getQuantidade());  
+        assertEquals(0, troco.papeisMoeda[3].getQuantidade());  
+        assertEquals(0, troco.papeisMoeda[2].getQuantidade());  
+        assertEquals(0, troco.papeisMoeda[1].getQuantidade());  
+        assertEquals(3, troco.papeisMoeda[0].getQuantidade());  // 3 x 2
     }
+
 }
+
+
+
+
+
