44e4360dd98ed08abb3e36ac0dbc4260e5362e12
Guilherme Zymberg <65249150+GuilhermeZymberg@users.noreply.github.com>
2023-10-26 21:18:46 -0300

    Update TrocoTest.java

--- Source Code Inspection/src/test/java/TrocoTest.java
+++ Source Code Inspection/src/test/java/TrocoTest.java
@@ -21,7 +21,10 @@ public class TrocoTest {
     public void testTrocoCalculation() {
         assertEquals(2, troco.papeisMoeda[5].getQuantidade());  
         assertEquals(1, troco.papeisMoeda[4].getQuantidade());
+        assertEquals(0, troco.papeisMoeda[3].getQuantidade());
+        assertEquals(0, troco.papeisMoeda[2].getQuantidade());
         assertEquals(0, troco.papeisMoeda[1].getQuantidade());
+        assertEquals(3, troco.papeisMoeda[0].getQuantidade());
     }
 
 }
